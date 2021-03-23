bf5eb3ed713138e744be4d540798e9c4b967f381
Viacheslav <brightsdayss@gmail.com>
2021-03-23 19:05:16 +0300

    check url, IE

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -18,15 +18,14 @@ const Main = () => {
   }
 
   const LoadList = ({ data }) => {
-      console.log(data);
       return(
           <ul className="gallery-list">
               {data &&
-              data.map((item) => (
+              data.map((item, index) => (
                   <li className="gallery-list__item"
                       key={item.url.substring(63)}>
                       <button className="gallery-list__close"
-                              onClick={e => e.target.parentNode.remove()}>X</button>
+                              onClick={() => setNames(names.filter(item => item !== names[index]))}>X</button>
                       <img className="gallery-list__image"
                            src={item.url}
                            alt="Image"/>
@@ -36,13 +35,16 @@ const Main = () => {
       );
   };
 
-  const addItem = (source) => {
-      const elem = [{
-          'url': source,
-          'height': 300,
-          'width': 600
-      }];
-      setNames(elem.concat(names));
+  const addItem = source => {
+      const img = new Image();
+      img.src = source;
+      img.onload = () => {
+          setNames([{'url': source}].concat(names));
+      };
+
+      // if (status === 'success') {
+      //     setNames([{'url': source}].concat(names));
+      // }
   };
 
   return(
@@ -50,7 +52,7 @@ const Main = () => {
           <div className="gallery-header">
               <TextField className="gallery-header__input"
                      id="standard-basic"
-                     label="введите url-адрес изображения или json-файла"
+                     label="введите url-адрес изображения"
                      onChange={e => setUrl(e.target.value)}/>
               <Button className="gallery-header__upload"
                       variant="contained"
