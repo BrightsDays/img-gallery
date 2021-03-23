626c64ba8457ac6d86901a0d381ae386e6215b86
Viacheslav <brightsdayss@gmail.com>
2021-03-23 14:46:51 +0300

    bem, add img

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,40 +1,67 @@
 import React, { useState } from 'react';
-import {Box, GridList, GridListTile, Container, Button} from '@material-ui/core';
+import { Button, TextField } from '@material-ui/core';
 import './App.sass';
 
 const Main = () => {
   const [names, setNames] = useState(null);
+  const [url, setUrl] = useState(null);
 
-  const fetchData = async () => {
-    const url = `https://don16obqbay2c.cloudfront.net/frontend-test-task/gallery-images.json`;
-    const settings = { method: "Get" };
+  if (!names) {
+      const url = `https://don16obqbay2c.cloudfront.net/frontend-test-task/gallery-images.json`;
+      const settings = { method: "Get" };
 
-    fetch(url, settings)
-        .then(res => res.json())
-        .then((json) => {
-          setNames(json.galleryImages);
-        });
-  };
+      fetch(url, settings)
+          .then(res => res.json())
+          .then((json) => {
+              setNames(json.galleryImages);
+          });
+  }
 
-  if (!names) { fetchData(); }
-
-  return(
-      <div className="gallery">
-          <div className="gallery__header">
-              <input></input>
-              <button variant="contained" color="primary" component="span">
-                  Upload
-              </button>
-          </div>
-          <ul>
-              {names &&
-              names.map((tile) => (
-                  <li>
-                      <button onClick={e => e.target.parentNode.remove()}>X</button>
-                      <img src={tile.url} alt={tile.width} />
+  const LoadList = ({ data }) => {
+      console.log(data);
+      return(
+          <ul className="gallery-list">
+              {data &&
+              data.map((item) => (
+                  <li className="gallery-list__item"
+                      key={item.url.substring(63)}>
+                      <button className="gallery-list__close"
+                              onClick={e => e.target.parentNode.remove()}>X</button>
+                      <img className="gallery-list__image"
+                           src={item.url}
+                           alt="Image"/>
                   </li>
               ))}
           </ul>
+      );
+  };
+
+  const addItem = (source) => {
+      const elem = [{
+          'url': source,
+          'height': 300,
+          'width': 600
+      }];
+      setNames(elem.concat(names));
+  };
+
+  return(
+      <div className="gallery">
+          <div className="gallery-header">
+              <TextField className="gallery-header__input"
+                     id="standard-basic"
+                     label="введите url-адрес изображения или json-файла"
+                     onChange={e => setUrl(e.target.value)}/>
+              <Button className="gallery-header__upload"
+                      variant="contained"
+                      color="primary"
+                      component="span"
+                      onClick={e => {
+                          e.preventDefault();
+                          addItem(url);
+                      }}>Загрузить</Button>
+          </div>
+          <LoadList data={names}/>
       </div>
   );
 };
