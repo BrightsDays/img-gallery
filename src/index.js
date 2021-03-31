9ebd4cf13ccdc4e1f8b0cfb691dd3cc304ff7ccf
Viacheslav <brightsdayss@gmail.com>
2021-03-31 12:21:02 +0300

    fix fit

--- src/index.js
+++ src/index.js
@@ -1,9 +1,12 @@
 import 'react-app-polyfill/ie9';
 import 'react-app-polyfill/ie11';
 import 'react-app-polyfill/stable';
+import objectFitImages from 'object-fit-images';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App/App';
+
+objectFitImages();
 
 ReactDOM.render(
   <React.StrictMode>
